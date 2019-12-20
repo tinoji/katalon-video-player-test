39c5718eedc39cda7cd7b6cd1d08653ed4660772
tinoji <pot5.sunen.on.the.earth.614@gmail.com>
2019-12-20 22:17:56 +0900

    Add: test suite and CircleCI config

---
+++ Test Suites/TestSuite01.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TestSuite01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c07052ad-2eec-4131-8b35-a2a6735dc2f4</testSuiteGuid>
+   <testCaseLink>
+      <guid>c2210653-4f4d-46d4-a5f8-7942aec908a7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Chrome</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
